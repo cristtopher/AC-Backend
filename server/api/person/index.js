8d85afbdd32f55316705c000f303f2c7cd269633
Mario Garces <mgarces132@gmail.com>
2016-12-22 02:47:08 -0300

    removed multi-space causing lint error

--- server/api/person/index.js
+++ server/api/person/index.js
@@ -5,7 +5,7 @@ var controller = require('./person.controller');
 
 var router = express.Router();
 
-router.get('/',  controller.index);
+router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.upsert);
